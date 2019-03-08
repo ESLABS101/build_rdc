dc5b5aece07ed091d60dae545e8c7fec0baad727
swap22 <sharma.sswapnil@gmail.com>
2019-03-08 16:09:37 +0530

    removed the region from area

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a09dec05ca3d8365a864d3561500c413.js"
+  "/precache-manifest.a250afc4d1e80b77bbbd71b8a2781ea6.js"
 );
 
 workbox.clientsClaim();
