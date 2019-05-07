2da59a32a574784a7bfde0bca7b7ed89d983712b
swap22 <sharma.sswapnil@gmail.com>
2019-05-07 16:02:36 +0530

    fix rf 6

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d68769e74806c534937f1e9d3834912.js"
+  "/precache-manifest.f3c5ac4d55ead1676bb2251ff8f98b7f.js"
 );
 
 workbox.clientsClaim();
