849fd528d082c7983d8273ade7e5c2e718d5aeca
swap22 <sharma.sswapnil@gmail.com>
2019-06-29 18:11:10 +0530

    fixed name of dashboard

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.690508a114b32539afc06069bd2ff489.js"
+  "/precache-manifest.df0932d5ca608f469018d6c08b631e38.js"
 );
 
 workbox.clientsClaim();
