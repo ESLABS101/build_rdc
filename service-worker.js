9cdb6a30c7817838f7c4d29fd4fb71532f314e82
swap22 <sharma.sswapnil@gmail.com>
2019-07-02 08:24:20 +0530

    fixed dashoard

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df0932d5ca608f469018d6c08b631e38.js"
+  "/precache-manifest.a9488fd3e42ecc333d6442fd99964dfe.js"
 );
 
 workbox.clientsClaim();
